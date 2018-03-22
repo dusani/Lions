666b4b776f4dd08360936b067d9741ca5be9d92a
Daniel Usani <daniel.e.usani@gmail.com>
2018-03-21 20:01:59 -0400

    Refactored server to Router and set-up for testing

---
+++ index.js
@@ -0,0 +1,5 @@
+var app = require("./server/server");
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
--- server/server.js
+++ server/server.js
@@ -3,82 +3,22 @@ var bodyParser = require("body-parser");
 var _ = require("lodash");
 var morgan = require("morgan");
 
+var lionRouter = require("./lions");
+
 var app = express();
-
-var lions = [];
-var id = 0;
-
-var updatedId = function(req, res, next) {
-  if (!req.body.id) {
-    id++;
-    req.body.id = id + "";
-  }
-  next();
-};
 
 app.use(morgan("dev"));
 app.use(express.static("client"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// now we need to mount the routes
+app.use("/lions", lionRouter);
+
 app.use((err, req, res, next) => {
-  if (err) {
-    res.status(500).send(error);
-  }
+    if (err) {
+        res.status(500).send(error);
+    }
 });
 
-app.param("id", function(req, res, next, id) {
-  var lion = _.find(lions, { id: id });
-
-  if (lion) {
-    req.lion = lion;
-    next();
-  } else {
-    res.send();
-  }
-});
-
-// routes
-app.get("/lions", (req, res) => {
-  res.json(lions);
-});
-
-app.get("/lions/:id", (req, res) => {
-  var lion = req.lion;
-  res.json(lion || {});
-});
-
-app.post("/lions", updatedId, (req, res) => {
-  var lion = req.body;
-  lions.push(lion);
-  res.json(lion);
-});
-
-app.put("/lions/:id", (req, res) => {
-  var update = req.body;
-  if (update.id) {
-    delete update.id;
-  }
-
-  var lion = _.findIndex(lions, { id: req.params.id });
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
-});
-
-app.delete("/lions/:id", (req, res) => {
-  var lion = _.findIndex(lions, { id: req.params.id });
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var deletedLion = lions[lion];
-    lions.splice(lion, 1);
-    res.json(deletedLion);
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+module.exports = app;
