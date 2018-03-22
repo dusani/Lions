5de01955702fe2ebc69a3e3798e762067611aaba
Daniel Usani <daniel.e.usani@gmail.com>
2018-03-21 21:28:41 -0400

    Created our first test and installed mocha test runner

--- server/spec.js
+++ server/spec.js
@@ -1,3 +1,35 @@
 var app = require("./server");
 var request = require("supertest");
 var expect = require("chai").expect;
+
+describe("[LIONS]", function() {
+    it("should get all lions", function(done) {
+        request(app)
+            .get("/lions")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end(function(err, res) {
+                expect(res.body).to.be.an("array");
+                done();
+            });
+    });
+
+    it("should create a lion", function(done) {
+        request(app)
+            .post("/lions")
+            .send({
+                name: "Mufasa",
+                age: 100,
+                pride: "Evil Cats",
+                gender: "Male"
+            })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(201)
+            .end(function(err, res) {
+                expect(res.body).to.be.an("object");
+                done();
+            });
+    });
+});
