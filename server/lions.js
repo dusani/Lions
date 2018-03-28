b4ff7b9fbc48c2897bafcc687d53e56b787733bd
Daniel Usani <daniel.e.usani@gmail.com>
2018-03-28 18:37:04 -0400

    Completed refactor of crud to MongoDB

--- server/lions.js
+++ server/lions.js
@@ -25,73 +25,104 @@ const Lion = require("../models/lions");
 
 // routes
 lionRouter.get("/", (req, res) => {
-    Lion.find((err, lion) => {
-        if (err) {
-            res.send(err);
-        }
+  Lion.find((err, lion) => {
+    if (err) {
+      res.send(err);
+    }
 
-        // mongoose sends info back in json format
-        res.send(lion);
-    });
+    // mongoose sends info back in json format
+    res.send(lion);
+  });
 });
 
 lionRouter.get("/:id", (req, res) => {
-    // var lion = req.lion;
-    // res.json(lion || {});
-    Lion.findById(req.params.id, (err, lion) => {
-        if (err) {
-            res.send(err);
-        }
-        res.send(lion);
-    });
+  // var lion = req.lion;
+  // res.json(lion || {});
+  Lion.findById(req.params.id, (err, lion) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(lion);
+  });
 });
 
 lionRouter.post("/", (req, res) => {
-    // var lion = req.body;
-    // lions.push(lion);
-    // res.json(lion);
+  // var lion = req.body;
+  // lions.push(lion);
+  // res.json(lion);
 
-    // receive the json Lion object
-    const lionObj = new Lion({
-        name: req.body.name,
-        age: req.body.age,
-        pride: req.body.pride,
-        gender: req.body.gender
-    });
+  // receive the json Lion object
+  const lionObj = new Lion({
+    name: req.body.name,
+    age: req.body.age,
+    pride: req.body.pride,
+    gender: req.body.gender
+  });
 
-    lionObj.save(err => {
-        if (err) {
-            res.send(err);
-        }
+  lionObj.save(err => {
+    if (err) {
+      res.send(err);
+    }
 
-        res.json({ message: "Lion Created" });
-    });
+    res.json({ message: "Lion Created" });
+  });
 });
 
 lionRouter.put("/:id", (req, res) => {
-    var update = req.body;
-    if (update.id) {
-        delete update.id;
+  // var update = req.body;
+  // if (update.id) {
+  //     delete update.id;
+  // }
+  //
+  // var lion = _.findIndex(lions, { id: req.params.id });
+  // if (!lions[lion]) {
+  //     res.send();
+  // } else {
+  //     var updatedLion = _.assign(lions[lion], update);
+  //     res.json(updatedLion);
+  // }
+  Lion.findById(req.params.id, (err, lion) => {
+    if (err) {
+      res.send(err);
+    }
+    if (req.body.name) {
+      lion.name = req.body.name;
+    }
+    if (req.body.age) {
+      lion.age = req.body.age;
+    }
+    if (req.body.pride) {
+      lion.pride = req.body.pride;
+    }
+    if (req.body.gender) {
+      lion.gender = req.body.gender;
     }
 
-    var lion = _.findIndex(lions, { id: req.params.id });
-    if (!lions[lion]) {
-        res.send();
-    } else {
-        var updatedLion = _.assign(lions[lion], update);
-        res.json(updatedLion);
-    }
+    lion.save(err => {
+      if (err) {
+        res.send(err);
+      }
+
+      res.json({ message: "Update the lion" });
+    });
+  });
 });
 
 lionRouter.delete("/:id", (req, res) => {
-    var lion = _.findIndex(lions, { id: req.params.id });
-    if (!lions[lion]) {
-        res.send();
-    } else {
-        var deletedLion = lions[lion];
-        lions.splice(lion, 1);
-        res.json(deletedLion);
+  // var lion = _.findIndex(lions, { id: req.params.id });
+  // if (!lions[lion]) {
+  //     res.send();
+  // } else {
+  //     var deletedLion = lions[lion];
+  //     lions.splice(lion, 1);
+  //     res.json(deletedLion);
+  // }
+  Lion.remove({ _id: req.params.id }, (err, lion) => {
+    if (err) {
+      res.send(err);
     }
+    res.json({ message: "Deleted the lion" });
+  });
 });
 
 module.exports = lionRouter;
